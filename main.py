4ee6fc7058a86877e183fa39e82af2494984058c
diixo <anatolii.podoba@gmail.com>
2023-07-08 16:38:15 +0300

    Compare signals

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scipy.io import wavfile
 import synt
 
 
-def musitic(fileName: str):
+def musitic(t, fileName: str, plot = False):
     plt.rcParams['figure.dpi'] = 72
     plt.rcParams['figure.figsize'] = (14, 8)
 
@@ -30,17 +30,18 @@ def musitic(fileName: str):
 
     time = np.arange(sound.shape[0]) / sound.shape[0] * length_in_s
 
-    plt.subplot(2,1,1)
-    plt.plot(time, sound[:,0], 'r')
-    plt.xlabel("time, s [left channel]")
-    plt.ylabel("signal, relative units")
-    plt.subplot(2,1,2)
-    plt.plot(time, sound[:,1], 'b')
-    plt.xlabel("time, s [right channel]")
-    plt.ylabel("signal, relative units")
-    plt.tight_layout()
-    plt.suptitle("Waveform:" + fileName)
-    plt.show()
+    if plot:
+        plt.subplot(2,1,1)
+        plt.plot(time, sound[:,0], 'r')
+        plt.xlabel("time, s [left channel]")
+        plt.ylabel("signal, relative units")
+        plt.subplot(2,1,2)
+        plt.plot(time, sound[:,1], 'b')
+        plt.xlabel("time, s [right channel]")
+        plt.ylabel("signal, relative units")
+        plt.tight_layout()
+        plt.suptitle("Waveform:" + fileName)
+        plt.show()
 
     ###############################################################
     # select separated signals from each channel:
@@ -48,7 +49,7 @@ def musitic(fileName: str):
     signal1 = sound[:,1]    # right channel
 
     # duration = sampFreq * time_s
-    time_s = 0.358
+    time_s = t #0.358
     #time_s = length_in_s
     duration = int(sampFreq * time_s)
 
@@ -62,13 +63,13 @@ def musitic(fileName: str):
             smooth[id0:id1] = max(signal0[id0], signal0[id1])
 
 
-    plt.plot(time[0:duration], signal0[0:duration], 'b')
-    #plt.plot(time[0:duration], smooth[0:duration], 'r')
-
-    plt.xlabel("time, s")
-    plt.ylabel("Signal, relative units")
-    plt.suptitle("Bump")
-    plt.show()
+    if plot:
+        plt.plot(time[0:duration], signal0[0:duration], 'b')
+        #plt.plot(time[0:duration], smooth[0:duration], 'r')
+        plt.xlabel("time, s")
+        plt.ylabel("Signal, relative units")
+        plt.suptitle("Bump")
+        plt.show()
 
     ###############################################################
     # frequency content with FFT:
@@ -92,18 +93,13 @@ def musitic(fileName: str):
     # It's interesting. Let's zoom in on the highest peaks:
 
     # Apply range[0:3000]
-    plt.plot(freq[:2000], fft_spectrum_abs[:2000])
-    plt.xlabel("frequency, Hz")
-    plt.ylabel("Amplitude, units")
-    plt.suptitle("FFT-spectr")
-    plt.show()
 
     # Recreate the original signal via an inverse FFT:
     original_signal = np.fft.irfft(fft_spectrum)
-    wavfile.write("test.wav", sampFreq, original_signal)
+    wavfile.write("track-" + str(t) + ".wav", sampFreq, original_signal)
 
     #################
-    return
+    return fft_spectrum_abs, freq
 
     for i, f in enumerate(fft_spectrum_abs):
         if f > 350: #looking at amplitudes of the spikes higher than 350 
@@ -127,8 +123,25 @@ def musitic(fileName: str):
 
 def main():
     #musitic('data/mix2.wav')
-    musitic('audio/short-full.wav')
+    spectrum_abs, freq = musitic(0.35, 'audio/short-full.wav')
+
+    spectrum_abs1, freq1 = musitic(0.291, 'audio/short-1-291.wav')
+    spectrum_abs2, freq2 = musitic(0.235, 'audio/short-2-235.wav')
+
+    spectrum_orig, freq0 = musitic(1.0, 'data/original_a3s.wav')
+
+    sz = 2000
+    plt.plot(freq[:sz], spectrum_abs[:sz])
+    #plt.plot(freq[:sz], spectrum_abs[:sz], 'g')
+    #plt.plot(freq1[:sz], spectrum_abs1[:sz], 'r')
+    #plt.plot(freq2[:sz], spectrum_abs2[:sz], 'b')
+    plt.xlabel("frequency, Hz")
+    plt.ylabel("Amplitude, units")
+    plt.suptitle("FFT-spectr")
+    plt.show()
+    
     pass
+
     
 if __name__ == "__main__":
     main()
